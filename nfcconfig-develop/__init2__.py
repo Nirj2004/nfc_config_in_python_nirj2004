5615ded2ccbdf74b24f9e82acdb1356809c2cc2f
Nirj2004 <75828293+Nirj2004@users.noreply.github.com>
2021-07-24 21:52:57 +0530

    Add files via upload

---
+++ nfcconfig-develop/__init2__.py
@@ -0,0 +1,14 @@
+"""@package py532lib
+This package contains the useful stuff to communicate with a PN532 NFC chip in Python.
+
+@author:  nirj2004 <me@nirj2004.in>
+@license: The source code within this file is licensed under the MIT license.
+          See LICENSE file for more information.
+
+Built for Python3, using Quick2Wire's Python APIs for the RaspberryPI.
+
+Losely inspired from Adafruit's PN532 I2C library.
+
+Pull requests are welcomed.
+
+"""
